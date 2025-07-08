40eee2a9fab76f58111bba76fda0b02b254fb20d
nezy2006 <ishimwesamnlson366@gmail.com>
2025-07-08 08:13:07 -0700

    seven

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(3 * str)
+print(str[:9])#!/usr/bin/python3
+str = "Holberton School"
+print(3 * str)
+print(str[:9])#!/usr/bin/python3
+str = "Holberton School"
+print(3 * str)
+print(str[:9])
