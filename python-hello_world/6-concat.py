e9b29b6e6f7f815ff0d5ed0808e63322d46e1593
nezy2006 <ishimwesamnlson366@gmail.com>
2025-07-08 08:51:47 -0700

    thirteen

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
-str2 = "Holberton School!"
-str2 = str2.replace("Holberton School", "Battery Street")
+str2 = "Battery Street!"
 str1 = f"{str1} {str2}"
 print(str1)
